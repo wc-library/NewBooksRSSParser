e9089e805c687222719d8a0ea90aa6b6214d80ad
bgarcia <brandon.garcia@my.wheaton.edu>
2015-07-30 14:35:30 -0500

    Fixes 1 [Uncaught exception 'String could not be parsed as XML']
    http://stackoverflow.com/a/12265956/5175218
    ^ provided a regex that fixed the issue.

--- classes/RSSFeed.php
+++ classes/RSSFeed.php
@@ -12,13 +12,22 @@
  * @author bgarcia
  */
 class RSSFeed {
-    protected $xml; // SimpleXML
+    protected $xml;
 
     public function __construct($url) {
         if (($xmlstr=file_get_contents($url))===FALSE)
-            throw new BadFunctionCallException("Failed to fetch content from url: \n$url");
-        //$this->xml = simplexml_load_string($xmlstr);
-        $this->xml = new SimpleXMLElement($xmlstr);
+            throw new RuntimeException("Failed to fetch content from url: \n$url");
+
+        $xmlstr = preg_replace ('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $xmlstr);
+        $xmlstr = \ForceUTF8\Encoding::toUTF8($xmlstr); // https://github.com/neitanod/forceutf8
+
+
+        $doc = new DOMDocument();
+        if ($doc->loadXML($xmlstr,LIBXML_PARSEHUGE|LIBXML_PEDANTIC) === FALSE) {
+            error_log("failed to loadXML from string");
+        } else if ( ($this->xml=simplexml_import_dom($doc)) === FALSE) {
+            error_log("failed to import DOM node into SimpleXML");
+        }
     }
 
     public function items() {
--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-define('CLASSES_DIR', 'classes/');
 
-// Automatically load undefined classes from |CLASSES_DIR|.
+$CLASSES_DIR = 'classes/';
+
+// Autoloader
 function __autoload( $className ) {
-	if (file_exists(CLASSES_DIR) && is_readable(CLASSES_DIR) && is_dir(CLASSES_DIR)) {
-        $filename = CLASSES_DIR . $className . '.php';
-        if (file_exists($filename) && is_readable($filename) && is_file($filename)) {
-            require($filename);
-        }
-	}
+    $parts = explode('\\', $className);
+    require "classes/".end($parts).".php";
 }
 
 $feed = new CarliBookFeed();
